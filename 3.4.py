7b8e2aac5a5173a04820865184d8053e3c626c6e
Trikis2 <105656225+Trikis2@users.noreply.github.com>
2022-05-23 15:02:49 +0300

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -1,8 +1,8 @@
 print('С использованием if')
-for i in range (1, 15):
+for i in range (1, 1000000):
     if i % 2 == 0:
         print (i)
 
 print('Без использования if')
-for a in range (2, 15, 2):
+for a in range (2, 1000000, 2):
     print(a)
