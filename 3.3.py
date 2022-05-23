cf3463652968e292a2a8d8c93e979fd4e911a67d
Trikis2 <105656225+Trikis2@users.noreply.github.com>
2022-05-23 14:52:07 +0300

    Add files via upload

---
+++ 3.3.py
@@ -0,0 +1,12 @@
+lst = [10, 17, 13, 44, 23, 88, 100, 99]
+count = 0
+print('Четные числа')
+for i in lst:
+    if i % 2 == 0:
+        print(i)
+
+print('Нечетные числа')
+for a in lst:
+    if a % 2 != 0:
+        print(a)
+
