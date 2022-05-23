cc1a4dd281f15ea642408255e784dc9549753e63
Trikis2 <105656225+Trikis2@users.noreply.github.com>
2022-05-23 12:04:59 +0300

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,3 @@
+first_number = int (input("Введите первое число: "))
+second_number = int (input("Введите второе чтсло: "))
+print("Среднее арифметическое двух чисел:", (first_number+second_number)/2)
